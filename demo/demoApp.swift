cdce57fb9f7113b70f2527898a06fae6be69437e
Kevin Furjan <kevin.furjan@ericsson.com>
2021-11-28 09:38:37 +0100

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Ericsson on 28.11.2021..
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
